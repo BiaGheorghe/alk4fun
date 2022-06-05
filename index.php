6f00bf1fceb927e8e4cf81acc3b359710f1ed39e
BiaGheorghe <61453241+BiaGheorghe@users.noreply.github.com>
2022-06-05 20:21:24 +0300

    index upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Info Page</title>
+    <link rel="stylesheet" href="ui/CSS/loginRegisterStyle.css">
+    <link rel="stylesheet" href="ui/CSS/style.css" />
+</head>
+<body>
+    <form class="login-logout-form" action="ui/goHome.php" method="post">
+        <p class="message">
+            Not Registered?
+        </p>
+        <button class="btn">I understand</button>
+    </form>
+</main>
+</body>
+</html>
